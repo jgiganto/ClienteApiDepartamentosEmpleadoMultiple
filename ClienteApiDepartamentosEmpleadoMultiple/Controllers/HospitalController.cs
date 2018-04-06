b89125db903b86638b9d9a66c0511730b28b4754
jgiganto <jgiganto1@hotmail.com>
2018-04-06 14:19:11 +0200

    a

---
+++ ClienteApiDepartamentosEmpleadoMultiple/Controllers/HospitalController.cs
@@ -0,0 +1,45 @@
+﻿using ClienteApiDepartamentosEmpleadoMultiple.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClienteApiDepartamentosEmpleadoMultiple.Controllers
+{
+    public class HospitalController : Controller
+    {
+        ModeloHospital modelo;
+        public HospitalController()
+        {
+            this.modelo = new ModeloHospital();
+        }
+        // GET: Hospital
+        public async Task<ActionResult> Departamentos()
+        {
+            List<Departamentos> departamentos = await modelo.GetDepartamento();
+            return View(departamentos);
+        }
+        public ActionResult Empleados()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> Empleados(List<int> departamentos)
+        {
+            List<Empleado> empleados = await modelo.GetEmpleadosDepartamentos(departamentos);
+            return View(empleados);
+        }
+
+        
+        [HttpPost]
+        public async Task<ActionResult> IncrementoSalarial(List<int> empno, int incremento)
+        {
+            List<Empleado> empleados = await modelo.IncrementoSalarial(empno, incremento);
+            return RedirectToAction("Departamentos");
+            //return View(empleados);
+        }
+
+    }
+}
